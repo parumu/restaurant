3cf599bb711485fe3c244c5810271656b423cb75
Kz <parumu4github@gmail.com>
2020-11-29 17:28:33 +0900

    add working client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,135 +1,180 @@
-fn main() {
-  println!("hhee")
+use application::{
+  item::Item,
+  api::AddItemParam,
+};
+use std::{
+  thread,
+  thread::JoinHandle,
+  time::Duration,
+};
+use reqwest::blocking::Client as HttpClient;
+use simple_logger::SimpleLogger;
+use log::{
+  info,
+  error,
+  warn,
+  LevelFilter,
+};
+
+/*
+  curl -X POST -H "Content-Type: application/json" -d '{"item_names":["ramen"] }' http://localhost:8888/v1/table/0/items
+  curl http://localhost:8888/v1/table/0/items
+*/
+
+struct Client {
+  id: usize,
+  http_client: HttpClient,
+  base_url: String,
 }
-// #[macro_use] extern crate log;
 
-// use restaurant::{
-//   item::Item,
-//   api::AddItemParam,
-//   http_server::build_rocket,
-//   clock::utc_clock::UtcClock,
-// };
-// use std::{
-//   thread,
-//   thread::JoinHandle,
-//   time::Duration,
-//   sync::Arc,
-// };
-// use reqwest::blocking::Client;
+impl Client {
+  pub fn new(id: usize) -> Client {
+    let t = Client {
+      id,
+      http_client: HttpClient::new(),
+      base_url: "http://localhost:8888/v1".to_string(),
+    };
+    t.wait_until_server_is_ready();
+    info!("{}: Server is ready", id);
+    t
+  }
 
-// struct Tablet {
-//   client: Client,
-//   base_url: String,
-// }
+  pub fn wait_until_server_is_ready(&self) {
+    let url = format!("{}/table/0/items", self.base_url);
+    info!("{}: Waiting for server to be ready...", self.id);
+    loop {
+      if let Ok(_) = self.http_client.get(&url).send() {
+        break;
+      }
+      println!(".");
+      thread::sleep(Duration::from_millis(500));
+    }
+  }
 
-// impl Tablet {
-//   pub fn new() -> Tablet {
-//     let t = Tablet {
-//       client: Client::new(),
-//       base_url: "http://localhost:8888/v1".to_string(),
-//     };
-//     t.wait_until_server_is_ready();
-//     info!("Server is ready");
-//     t
-//   }
+  pub fn get_item(&self, table_id: usize, uuid: &str) -> Result<Item, reqwest::Error> {
+    let url = format!("{}/table/{}/item/{}", self.base_url, table_id, uuid);
+    match self.http_client
+      .get(&url)
+      .send()
+    {
+      Ok(res) => {
+        res.text().map(|s| serde_json::from_str::<Item>(&s).unwrap())
+      },
+      Err(err) => {
+        error!("Failed to get item {} of table {}: {:?}", uuid, table_id, err);
+        Err(err)
+      }
+    }
+  }
 
-//   pub fn wait_until_server_is_ready(&self) {
-//     let url = format!("{}/table/0/items", self.base_url);
-//     println!("Waiting for server to be ready...");
-//     loop {
-//       if let Ok(_) = self.client.get(&url).send() {
-//         break;
-//       }
-//       println!(".");
-//       thread::sleep(Duration::from_millis(500));
-//     }
-//   }
+  pub fn get_all_items(&self, table_id: usize) -> Result<Vec<Item>, reqwest::Error> {
+    let url = format!("{}/table/{}/items", self.base_url, table_id);
+    match self.http_client
+      .get(&url)
+      .send()
+    {
+      Ok(res) => {
+        res.text().map(|s| serde_json::from_str::<Vec<Item>>(&s).unwrap())
+      },
+      Err(err) => {
+        error!("Failed to get all items of table {}: {:?}", table_id, err);
+        Err(err)
+      }
+    }
+  }
 
-//   pub fn get_item(&self, table_id: usize, uuid: &str) -> Result<Item, reqwest::Error> {
-//     let url = format!("{}/table/{}/item/{}", self.base_url, table_id, uuid);
-//     let res = self.client
-//       .get(&url)
-//       .send()
-//       .unwrap();
+  pub fn remove_item(&self, table_id: usize, uuid: &str) -> Result<(), reqwest::Error> {
+    let url = format!("{}/table/{}/item/{}", self.base_url, table_id, uuid);
+    match self.http_client
+      .delete(&url)
+      .send()
+    {
+      Ok(res) => {
+        res.text().map(|s| serde_json::from_str::<()>(&s).unwrap())
+      },
+      Err(err) => {
+        error!("Failed to remove item {:?} from table {}: {:?}", uuid, table_id, err);
+        Err(err)
+      }
+    }
+  }
 
-//     res.text().map(|s| serde_json::from_str::<Item>(&s).unwrap())
-//   }
+  pub fn add_item(&self, table_id: usize, item_names: Vec<String>) -> Result<Vec<Item>, reqwest::Error> {
+    let url = format!("{}/table/{}/items", self.base_url, table_id);
+    let req = AddItemParam {
+      item_names: item_names.clone(),
+    };
+    let req_json = serde_json::to_string(&req).unwrap();
+    match self.http_client
+      .post(&url)
+      .body(req_json)
+      .send()
+    {
+      Ok(res) => {
+        res.text().map(|s| serde_json::from_str::<Vec<Item>>(&s).unwrap())
 
-//   pub fn get_all_items(&self, table_id: usize) -> Result<Vec<Item>, reqwest::Error> {
-//     let url = format!("{}/table/{}/items", self.base_url, table_id);
-//     let res = self.client
-//       .get(&url)
-//       .send()
-//       .unwrap();
+      },
+      Err(err) => {
+        error!("Failed to add item {:?} to table {}: {:?}", item_names, table_id, err);
+        Err(err)
+      }
+    }
+  }
+}
 
-//     res.text().map(|s| serde_json::from_str::<Vec<Item>>(&s).unwrap())
-//   }
+fn start_client(id: usize) {
+  log::info!("{}: Started", id);
+  let cli = Client::new(id);
+  let table_id = id;
 
-//   pub fn remove_item(&self, table_id: usize, uuid: &str) -> Result<(), reqwest::Error> {
-//     let url = format!("{}/table/{}/item/{}", self.base_url, table_id, uuid);
-//     let res = self.client
-//       .delete(&url)
-//       .send()
-//       .unwrap();
+  loop {
+    // add 1 item to table 0
+    let items2add = vec![format!("{}-ramen", id)];
+    match cli.add_item(id, items2add.clone()) {
+      Ok(items) => {
+        info!("{}: Added items {:?} to table {}", id, items, table_id);
+        let uuid = &items[0].uuid;
 
-//     res.text().map(|s| serde_json::from_str::<()>(&s).unwrap())
-//   }
+        match cli.get_item(id, uuid) {
+          Ok(item) => info!("{}: Got item {} of table {}: {:?}", id, uuid, table_id, item),
+          Err(err) => error!("{}: Failed to get item {} of table {}: {:?}", id, uuid, table_id, err),
+        }
 
-//   pub fn add_item(&self, table_id: usize, item_names: Vec<String>) -> Result<Vec<Item>, reqwest::Error> {
-//     let url = format!("{}/table/{}/items", self.base_url, table_id);
-//     let req = AddItemParam {
-//       item_names,
-//     };
-//     let req_json = serde_json::to_string(&req).unwrap();
-//     let res = self.client
-//       .post(&url)
-//       .body(req_json)
-//       .send()
-//       .unwrap();
+        match cli.get_all_items(id) {
+          Ok(items) => info!("{}: Got all items of table {}: {:?}", id, table_id, items),
+          Err(err) => error!("{}: Failed to get all items of table {}: {:?}", id, table_id, err),
+        }
 
-//     res.text().map(|s| serde_json::from_str::<Vec<Item>>(&s).unwrap())
-//   }
-// }
+        loop {
+          match cli.remove_item(id, uuid) {
+            Ok(_) => break,
+            Err(err) => {
+              warn!("{}: Retrying removal of {} in table {}: {:?}", id, uuid, table_id, err);
+              thread::sleep(Duration::from_millis(500));
+            },
+          }
+        }
+      },
+      Err(err) => {
+        error!("{}: Failed to add item {:?} to table {}: {:?}", id, items2add, table_id, err);
+      }
+    }
+  }
+}
 
-// fn start_tablet() {
-//   let tablet = Tablet::new();
+fn main() {
+  SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
 
-//   // add 1 item to table 0
-//   if let Ok(items) = tablet.add_item(0, vec!["ramen".to_string()]) {
-//     println!("Added items: {:?}", items);
+  let num_clients = 5;
 
-//     // get the added item in table 0
-//     if let Ok(item) = tablet.get_item(0, &items[0].uuid) {
-//       println!("Item of uuid {}: {:?}", items[0].uuid, item);
-//     }
+  let hs: Vec<JoinHandle<()>> = (0..num_clients).map(|i| {
+    thread::spawn(move || start_client(i))
+  }).collect();
 
-//     // get all items in table 0
-//     if let Ok(items) = tablet.get_all_items(0) {
-//       println!("Snapshot of table {}: {:?}", 0, items);
-//     }
-    
-//     // remove the added item in table 0
-//     if let Ok(()) = tablet.remove_item(0, &items[0].uuid) {
-//       println!("Removed item of uuid: {}", &items[0].uuid);
-//     }
-//     assert!(false);
-//   }
-// }
-
-// fn main() {
-//   let num_tablets = 2;
-
-//   // start server
-//   thread::spawn(|| {
-//     let clock = Arc::new(UtcClock());
-//     build_rocket(clock).launch();
-//   });
-
-//   let handles: Vec<JoinHandle<()>> = (0..num_tablets).map(|_|  
-//     thread::spawn(|| start_tablet())
-//   ).collect();
-
-//   for handle in handles {
-//     handle.join().unwrap();
-//   }
-// }
+  for h in hs {
+    if let Err(err) = h.join() {
+      error!("Client paniced: {:?}", err);
+      return;
+    }
+  }
+}
